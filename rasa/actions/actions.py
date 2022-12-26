9cb2822b6a910591a9853df9e8b17e2a1c21e72f
PiotrPC <piotr98nowakowski@gmail.com>
2022-12-26 14:42:36 +0100

    added action classes for weather forecast and current situation needs testing and domain and nlu

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -15,6 +15,9 @@ import keyboard
 import pyautogui
 import webbrowser
 import time
+from pyowm import OWM
+import requests
+
 #
 class ActionHelloWorld(Action):
 
@@ -77,7 +80,6 @@ class OpenYouTube(Action):
             user_input = user_input.split('youtube')[1]
         if  'YouTube' in user_input:
             user_input = user_input.split('YouTube')[1]
-
         
         # Open YouTube in a new tab
         webbrowser.open_new_tab("https://www.youtube.com/results?search_query={}".format(user_input))
@@ -85,5 +87,98 @@ class OpenYouTube(Action):
         # Send a message to the user
         dispatcher.utter_message(text="Searching for '{}' on YouTube".format(user_input))
 
+
+class OpenWeatherMapClient:
+    def __init__(self, api_key):
+        self.owm_client = OWM(api_key)
+
+    def get_current_weather(self, location):
+        # Send a request to the OpenWeatherMap API to get the current weather for the given location
+        weather = self.owm_client.weather_at_place(location).get_weather()
+        # Return the weather data
+        return weather
+    
+    def get_user_location(self):
+        # Set up the API URL and request headers
+        api_url = "https://api.ipgeolocation.io/ipgeo"
+        headers = {"API-Key": '2ae49291af8d453a917666db8d5ab3be'}
+
+        # Send a request to the API to get the location of the user's device
+        response = requests.get(api_url, headers=headers)
+
+        # Parse the response data
+        data = response.json()
+
+        # Print the city and county
+        return { 'city': data['city'], 'country': data['county']}
+
+
+class GetWeatherAction(Action):
+    def __init__(self):
+        # Set up the OpenWeatherMap API client
+        self.weather_client = OpenWeatherMapClient(api_key='2988f66310cb9fb07835c49b29d9b685')
+
+    def name(self):
+        return "action_get_weather"
+
+    def run(self, dispatcher, tracker, domain):
+        # Get the location from the user's message
+        location = self.weather_client.get_user_location()
+        location = location['city'] + ", " + location['country']
+ 
+        # Use the OpenWeatherMap API to get the current weather for the location
+        weather = self.weather_client.get_current_weather(location)
+
+        # Build a message with the weather information
+        message = f"The current weather in {location} is {weather.temperature('celsius')['temp']}°C and {weather.detailed_status}."
+
+        # Check if it is raining and include that information in the message
+        if 'rain' in weather.weather:
+            message += " It is currently raining."
+        else:
+            message += " It is not currently raining."
+
+        # Check if it is cloudy and include that information in the message
+        if 'clouds' in weather.weather:
+            message += " The sky is cloudy."
+        else:
+            message += " The sky is clear."
+
+        dispatcher.utter_message(text=message)
+
+
+class GetWeatherAction(Action):
+    def __init__(self):
+        # Set up the OpenWeatherMap API client
+        self.weather_client = pyowm.OWM('2988f66310cb9fb07835c49b29d9b685')
+
+    def name(self):
+        return "action_get_tommorow_weather"
+
+    def run(self, dispatcher, tracker, domain):
+        # Get the location from the user's message
+        location = self.weather_client.get_user_location()
+        location = location['city'] + ", " + location['country']
+
+        # Use the OpenWeatherMap API to get the current weather for the location
+        weather = self.weather_client.weather_at_place(location).get_weather()
+
+        # Use the OpenWeatherMap API to get the forecast for the location
+        forecast = self.weather_client.three_hours_forecast(location)
+
+        # Get the start and end timestamps for the next day
+        tomorrow = pyowm.timeutils.tomorrow()
+        end_of_tomorrow = pyowm.timeutils.end_of_tomorrow()
+
+        # Get the forecast data for the next day
+        forecast_tomorrow = forecast.get_weathers_between(tomorrow, end_of_tomorrow)
+
+        # Get the forecast for the next day
+        forecast_tomorrow = forecast_tomorrow[0]
+
+        # Include the forecast for the next day in the message
+        message += f" The forecast for tomorrow is {forecast_tomorrow.get_temperature('celsius')['max']}°C and {forecast_tomorrow.get_detailed_status()}."
+
+        dispatcher.utter_message(text=message)
 
 
